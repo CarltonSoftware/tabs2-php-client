55cb7cc164d6a75b87aeb3c42c4a2689bedcda95
Alex Wyett <alex.wyett@gmail.com>
2014-08-24 22:57:30 +0100

    Added CustomerCollection object to allow for filtering of customer objects.

---
+++ src/tabs/actor/CustomerCollection.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Tabs Rest API Customer collection object.
+ *
+ * PHP Version 5.4
+ *
+ * @category  Tabs_Client
+ * @package   Tabs
+ * @author    Carlton Software <support@carltonsoftware.co.uk>
+ * @copyright 2014 Carlton Software
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @link      http://www.carltonsoftware.co.uk
+ */
+
+namespace tabs\actor;
+
+/**
+ * Tabs Rest API Customer collection object.
+ *
+ * @category  Tabs_Client
+ * @package   Tabs
+ * @author    Carlton Software <support@carltonsoftware.co.uk>
+ * @copyright 2014 Carlton Software
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version   Release: 1
+ * @link      http://www.carltonsoftware.co.uk
+ */
+class CustomerCollection extends \tabs\core\Collection
+{
+    /**
+     * Return an array of customer objects.  This object will need to be
+     * instantiated and the method fetch will need to be called before this will
+     * return any elements.
+     * 
+     * @return \tabs\actor\Customer[]
+     */
+    public function getElements()
+    {
+        return $this->elements;
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    public function getRoute()
+    {
+        return 'customer';
+    }
+}
