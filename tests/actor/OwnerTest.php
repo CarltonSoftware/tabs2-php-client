c3927367fdfeaad12efb3fccd007fb4e97dc64c5
cdobson <c.dobson@tocc.co.uk>
2014-10-07 09:26:19 +0100

    Added owner actor functionality

---
+++ tests/actor/OwnerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+$file = dirname(__FILE__)
+    . DIRECTORY_SEPARATOR . '..'
+    . DIRECTORY_SEPARATOR . '..'
+    . DIRECTORY_SEPARATOR . 'tests'
+    . DIRECTORY_SEPARATOR . 'client'
+    . DIRECTORY_SEPARATOR . 'ApiClientClassTest.php';
+require_once $file;
+
+class OwnerTest extends ApiClientClassTest
+{
+
+}
