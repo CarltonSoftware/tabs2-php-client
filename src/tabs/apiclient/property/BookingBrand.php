0c9f218fbdc21f1e4fe3bb1cb9b0364538a05048
Alex Wyett <alex.wyett@gmail.com>
2014-11-10 16:52:15 +0000

    New brand objects.

---
+++ src/tabs/apiclient/property/BookingBrand.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Tabs Rest API Property Booking Brand object.
+ *
+ * PHP Version 5.4
+ *
+ * @category  Tabs_Client
+ * @package   Tabs
+ * @author    Carlton Software <support@carltonsoftware.co.uk>
+ * @copyright 2014 Carlton Software
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @link      http://www.carltonsoftware.co.uk
+ */
+
+namespace tabs\apiclient\property;
+
+/**
+ * Tabs Rest API Property Booking Brand object.
+ *
+ * @category  Tabs_Client
+ * @package   Tabs
+ * @author    Carlton Software <support@carltonsoftware.co.uk>
+ * @copyright 2014 Carlton Software
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version   Release: 1
+ * @link      http://www.carltonsoftware.co.uk
+ */
+class BookingBrand extends Brand
+{
+    /**
+     * @inheritDoc
+     */
+    public function toArray()
+    {
+        return array(
+            'status' => $this->getStatus()->getName()
+        );
+    }
+}
