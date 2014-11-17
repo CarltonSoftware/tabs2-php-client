348ec6c32223c98a56e4d841357b079792a99c65
Alex Wyett <alex.wyett@gmail.com>
2014-11-17 11:04:30 +0000

    New marketing brand object.

---
+++ src/tabs/apiclient/brand/MarketingBrand.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Tabs Rest API Marketing Brand object.
+ *
+ * PHP Version 5.4
+ *
+ * @category  Tabs_Client
+ * @package   Tabs
+ * @author    Carlton Software <support@carltonsoftware.co.uk>
+ * @copyright 2014 Carlton Software
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @link      http://www.carltonsoftware.co.uk
+ */
+
+namespace tabs\apiclient\brand;
+
+/**
+ * Tabs Rest API Marketing Brand object.
+ *
+ * @category  Tabs_Client
+ * @package   Tabs
+ * @author    Carlton Software <support@carltonsoftware.co.uk>
+ * @copyright 2014 Carlton Software
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version   Release: 1
+ * @link      http://www.carltonsoftware.co.uk
+ */
+class MarketingBrand extends Brand
+{
+    /**
+     * @inheritDoc
+     */
+    public function toArray()
+    {
+        return array();
+    }
+}
