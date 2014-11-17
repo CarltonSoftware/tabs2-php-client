627ade8162e3916c986215d1f331be6f0d175bd4
Alex Wyett <alex.wyett@gmail.com>
2014-11-17 11:04:05 +0000

    New branding object.

---
+++ src/tabs/apiclient/brand/Branding.php
@@ -0,0 +1,110 @@
+<?php
+
+/**
+ * Tabs Rest API Booking Brand object.
+ *
+ * PHP Version 5.4
+ *
+ * @category  Tabs_Client
+ * @package   Tabs
+ * @author    Carlton Software <support@carltonsoftware.co.uk>
+ * @copyright 2014 Carlton Software
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @link      http://www.carltonsoftware.co.uk
+ */
+
+namespace tabs\apiclient\brand;
+
+/**
+ * Tabs Rest API Booking Brand object.
+ *
+ * @category  Tabs_Client
+ * @package   Tabs
+ * @author    Carlton Software <support@carltonsoftware.co.uk>
+ * @copyright 2014 Carlton Software
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version   Release: 1
+ * @link      http://www.carltonsoftware.co.uk
+ * 
+ * @method integer  getId()            Returns the brand id
+ * @method Branding setId(integer $id) Sets the brand id
+ * 
+ * @method BrandingGroup  getBrandinggroup()  Return the branding group
+ * @method MarketingBrand getMarketingbrand() Return the marketing brand
+ * @method BookingBrand   getBookingbrand()   Return the booking brand
+ */
+class Branding extends \tabs\apiclient\core\Base
+{
+    /**
+     * Branding Id
+     * 
+     * @var integer
+     */
+    protected $id;
+    
+    /**
+     * The branding group (accounting brand)
+     * 
+     * @var BrandingGroup
+     */
+    protected $brandinggroup;
+    
+    /**
+     * The marketing brand
+     * 
+     * @var MarketingBrand
+     */
+    protected $marketingbrand;
+    
+    /**
+     * The booking brand
+     * 
+     * @var BookingBrand
+     */
+    protected $bookingbrand;
+    
+    /**
+     * Set the branding group
+     * 
+     * @param array|stdClass|BrandingGroup $grp Branding group
+     * 
+     * @return \tabs\apiclient\brand\Branding
+     */
+    public function setBrandinggroup($grp)
+    {
+        $group = BrandingGroup::factory($grp);
+        $this->brandinggroup = $group->setParent($this);
+        
+        return $this;
+    }
+    
+    /**
+     * Set the marketing brand
+     * 
+     * @param array|stdClass|MarketingBrand $grp MarketingBrand group
+     * 
+     * @return \tabs\apiclient\brand\Branding
+     */
+    public function setMarketingbrand($grp)
+    {
+        $group = MarketingBrand::factory($grp);
+        $this->marketingbrand = $group->setParent($this);
+        
+        return $this;
+    }
+    
+    /**
+     * Set the booking brand
+     * 
+     * @param array|stdClass|BookingBrand $grp BookingBrand group
+     * 
+     * @return \tabs\apiclient\brand\Branding
+     */
+    public function setBookingbrand($grp)
+    {
+        $group = BookingBrand::factory($grp);
+        $this->bookingbrand = $group->setParent($this);
+        
+        return $this;
+    }
+}
