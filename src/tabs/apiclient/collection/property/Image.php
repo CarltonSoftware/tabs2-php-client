50509474eaa7f45f5da895ceae20f7126e4c6f91
Alex Wyett <alex.wyett@gmail.com>
2015-02-03 15:32:19 +0000

    Added image collection.

---
+++ src/tabs/apiclient/collection/property/Image.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Tabs Rest API Property image collection object.
+ *
+ * PHP Version 5.4
+ *
+ * @category  Tabs_Client
+ * @package   Tabs
+ * @author    Carlton Software <support@carltonsoftware.co.uk>
+ * @copyright 2014 Carlton Software
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @link      http://www.carltonsoftware.co.uk
+ */
+
+namespace tabs\apiclient\collection\property;
+
+/**
+ * Tabs Rest API Property image collection object.
+ *
+ * @category  Tabs_Client
+ * @package   Tabs
+ * @author    Carlton Software <support@carltonsoftware.co.uk>
+ * @copyright 2014 Carlton Software
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version   Release: 1
+ * @link      http://www.carltonsoftware.co.uk
+ */
+class Image extends \tabs\apiclient\collection\Collection
+{
+    /**
+     * Return an array of Property image objects.  This object will need to be
+     * instantiated and the method fetch will need to be called before this will
+     * return any elements.
+     *
+     * @return \tabs\apiclient\core\Image[]
+     */
+    public function getElements()
+    {
+        return $this->elements;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getRoute()
+    {
+        return $this->getElementParent()->getUpdateUrl() . '/image';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getElementClass()
+    {
+        return '\tabs\apiclient\core\Image';
+    }
+}
