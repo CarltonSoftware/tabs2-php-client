ad3d9b5df24bffaf08569561536d84a47f828da9
Alex Wyett <alex.wyett@gmail.com>
2015-02-16 13:08:51 +0000

    Image tests.

--- tests/Fixtures.php
+++ tests/Fixtures.php
@@ -299,7 +299,7 @@ class Fixtures
         $hybrid = Fixtures::getPropertyHybridAttribute();
         $property->addAttribute($hybrid);
         
-        $img = Fixtures::getImage();
+        $img = Fixtures::getPropertyImage();
         $property->addImage($img);
         
         return $property;
@@ -661,5 +661,18 @@ class Fixtures
             ->setWidth(100);
         
         return $img;
+    }
+    
+    /**
+     * Create a new property image
+     * 
+     * @return \tabs\apiclient\property\Image
+     */
+    public static function getPropertyImage()
+    {
+        $propImage = new tabs\apiclient\property\Image();
+        $propImage->setId(1)->setImage(Fixtures::getImage());
+        
+        return $propImage;
     }
 }
--- tests/core/ImageClassTest.php
+++ tests/core/ImageClassTest.php
@@ -11,6 +11,26 @@ require_once $file;
 class ImageClassTest extends ApiClientClassTest
 {
     /**
+     * Static img
+     * 
+     * @var \tabs\apiclient\core\Image
+     */
+    static $img;
+    
+    /**
+     * Sets up the tests
+     *
+     * @return void
+     */
+    public static function setUpBeforeClass()
+    {
+        parent::setUpBeforeClass();
+        
+        self::$img = new \tabs\apiclient\core\Image();
+        self::$img->setDataFromPath('http://placehold.it/350x150');
+    }
+    
+    /**
      * Test a image object
      * 
      * @return void
@@ -28,5 +48,44 @@ class ImageClassTest extends ApiClientClassTest
             $img->getUrl()
         );
         $this->assertEquals('/image/1', $img->getPath());
+    }
+    
+    /**
+     * Test adding a new image
+     * 
+     * @return void
+     */
+    public function testImageRequest()
+    {
+        $this->assertEquals(350, self::$img->getWidth());
+        $this->assertEquals(150, self::$img->getHeight());
+    }
+    
+    /**
+     * Test resizing a new image
+     * 
+     * @depends testImageRequest
+     * 
+     * @return void
+     */
+    public function testImageResizeRequest()
+    {
+        self::$img->resize(100, 100);
+        $this->assertEquals(100, self::$img->getWidth());
+        $this->assertEquals(100, self::$img->getHeight());
+    }
+    
+    /**
+     * Test cropping a new image
+     * 
+     * @depends testImageResizeRequest
+     * 
+     * @return void
+     */
+    public function testImageCropRequest()
+    {
+        self::$img->crop(50, 50);
+        $this->assertEquals(50, self::$img->getWidth());
+        $this->assertEquals(50, self::$img->getHeight());
     }
 }
