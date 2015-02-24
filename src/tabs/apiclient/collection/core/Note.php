1e1dc81e4b8cb167ac1b13b74b937b8c3dadfcbc
Alex Wyett <alex.wyett@gmail.com>
2015-02-24 11:22:43 +0000

    Customer arrays are now collection objects.

--- src/tabs/apiclient/collection/core/Note.php
+++ src/tabs/apiclient/collection/core/Note.php
@@ -1,8 +1,58 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Tabs Rest API Note collection object.
+ *
+ * PHP Version 5.4
+ *
+ * @category  Tabs_Client
+ * @package   Tabs
+ * @author    Carlton Software <support@carltonsoftware.co.uk>
+ * @copyright 2014 Carlton Software
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @link      http://www.carltonsoftware.co.uk
  */
 
+namespace tabs\apiclient\collection\core;
+
+/**
+ * Tabs Rest API Note collection object.
+ *
+ * @category  Tabs_Client
+ * @package   Tabs
+ * @author    Carlton Software <support@carltonsoftware.co.uk>
+ * @copyright 2014 Carlton Software
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version   Release: 1
+ * @link      http://www.carltonsoftware.co.uk
+ */
+class Note extends \tabs\apiclient\collection\Collection
+{
+    /**
+     * Return an array of note objects.  This object will need to be
+     * instantiated and the method fetch will need to be called before this will
+     * return any elements.
+     *
+     * @return \tabs\apiclient\actor\Note[]
+     */
+    public function getElements()
+    {
+        return $this->elements;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getRoute()
+    {
+        return 'note';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getElementClass()
+    {
+        return '\tabs\apiclient\core\Note';
+    }
+}
