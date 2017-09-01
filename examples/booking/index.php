ff09a485e67b47826973a56da112d0fd33a95762
Alex Wyett <alex@carltonsoftware.co.uk>
2017-09-01 17:01:06 +0100

    Fixed deposit amount example

--- examples/booking/index.php
+++ examples/booking/index.php
@@ -31,7 +31,7 @@ try {
 
         if (!$booking->isConfirmed() && $booking->isProvisional()) {
             ?>
-        <p>Deposit amount: <?php echo $booking->getProvisionalbooking()->getDepositamount(); ?></p>
+        <p>Deposit amount: <?php echo $booking->getProvisionalbooking()->getDeposit(); ?></p>
         <p>Deposit due: <?php echo $booking->getProvisionalbooking()->getDepositduedate()->format('d F Y'); ?></p>
         <p><a href="confirm-booking.php?id=<?php echo $booking->getId(); ?>">Confirm booking</a></p>
             <?php
