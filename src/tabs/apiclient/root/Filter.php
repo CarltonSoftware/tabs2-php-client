26955b98cd9e2bb4cc44e4c9272317da99f6ebbb
Jon Beverley <jon@csdl.biz>
2018-11-15 16:10:09 +0000

    Small optimisations

--- src/tabs/apiclient/root/Filter.php
+++ src/tabs/apiclient/root/Filter.php
@@ -14,13 +14,12 @@ use tabs\apiclient\Collectionable;
  * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
  * @version   Release: 1
  * @link      http://www.carltonsoftware.co.uk
- * 
+ *
  * @method string getDescription() Return the description
  * @method array  getOperators()   Return the operators
  * @method string getType()        Return the type
  * @method string getFilter()        Return the filter key
- * @method string getEndpoint()    Return the applicable endpoint
- * 
+ *
  * @method Filter setDescription(string $str) Set the description
  * @method Filter setOperators(array $op)     Set the operators
  * @method Filter setType(string $str)        Set the type
@@ -31,54 +30,64 @@ class Filter implements Collectionable
 {
     use \tabs\apiclient\StateTrait;
     use \tabs\apiclient\FactoryTrait;
-    
+
     /**
      * Description
-     * 
+     *
      * @var string
      */
     protected $description = '';
-    
+
     /**
      * Operators
-     * 
+     *
      * @var array
      */
     protected $operators = array();
-    
+
     /**
      * Type
-     * 
+     *
      * @var string
      */
     protected $type = '';
-    
+
     /**
      * Filter
-     * 
+     *
      * @var string
      */
     protected $filter = '';
-    
+
     /**
      * Endpoint
-     * 
+     *
      * @var string
      */
     protected $endpoint = '';
-    
+
     /**
      * Select Option
-     * 
+     *
      * @var string
      */
     protected $select;
-    
+
     /**
      * @inheritDoc
      */
     public function getCreateUrl()
     {
         return '';
+    }
+
+    /**
+     * Return the applicable endpoint
+     *
+     * @return string
+     */
+    public function getEndpoint()
+    {
+        return $this->endpoint;
     }
 }
