38fd85f296046dccc4baa34277446cea1a6af835
wgeorgecrisan <g.crisan@tabs-software.co.uk>
2019-08-06 16:16:55 +0100

    fix tests

--- tests/BookingTest.php
+++ tests/BookingTest.php
@@ -9,7 +9,7 @@ require_once $file;
 
 class BookingTest extends ApiClientClassTest
 {
-    const BOOKING_ID = 119644;
+    const BOOKING_ID = 4276073;
 
     /**
      * Test booking get
@@ -36,14 +36,14 @@ class BookingTest extends ApiClientClassTest
     private function _validateBooking(tabs\apiclient\Booking $booking)
     {
         $this->assertEquals(self::BOOKING_ID, $booking->getId());
-        $this->assertEquals(893.58, $booking->getBasicPrice());
-        $this->assertEquals(932, $booking->getBrochurePrice());
+        $this->assertEquals(669, $booking->getBasicPrice());
+        $this->assertEquals(699, $booking->getBrochurePrice());
         $this->assertEquals(2, $booking->getExtras()->count());
-        $this->assertEquals(38.42, $booking->getIncludedExtrasPrice());
-        $this->assertEquals(30, $booking->getAdditionalExtrasPrice());
-        $this->assertEquals(50, $booking->getSecuritydeposit()->getAmount());
-        $this->assertEquals(50, $booking->getSecuritydeposit()->getOutstanding());
-        $this->assertEquals(0, $booking->getSecuritydeposit()->getRefundable());
+        $this->assertEquals(30, $booking->getIncludedExtrasPrice());
+        $this->assertEquals(36, $booking->getAdditionalExtrasPrice());
+        $this->assertEquals(20, $booking->getSecuritydeposit()->getAmount());
+        $this->assertEquals(0, $booking->getSecuritydeposit()->getOutstanding());
+        $this->assertEquals(20, $booking->getSecuritydeposit()->getRefundable());
         $this->assertEquals(0, $booking->getSecuritydeposit()->getRefunded());
     }
 }
