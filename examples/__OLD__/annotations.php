18431f37b1464b0bea2efaeff7e66ad74ef8041e
Alex <alex.wyett@gmail.com>
2020-05-26 15:22:41 +0100

    feat: TABS2-5456 Flesh out payment items and owner charges necessary for owner payments

--- examples/__OLD__/annotations.php
+++ examples/__OLD__/annotations.php
@@ -3,7 +3,7 @@
 // Include the connection
 require_once __DIR__ . '/../creating-a-new-connection.php';
 
-$class = new tabs\apiclient\booking\Voucher();
+$class = new tabs\apiclient\owner\PaymentItem();
 
 $ref = new ReflectionClass($class);
 $properties = array();
